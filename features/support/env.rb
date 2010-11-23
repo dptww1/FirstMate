e2e60d6133b4e487e317c8214f6262d64b575abc
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2010-11-23 10:04:28 -0500

    Bug fix to get rid of Rake deprecation warning

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,7 +16,8 @@ require 'cucumber/web/tableish'
 require 'capybara/rails'
 require 'capybara/cucumber'
 require 'capybara/session'
-require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
+#COMMENTED FOLLOWING LINE OUT PER https://github.com/jnicklas/capybara/issues/issue/87/#issue/87/comment/433959
+#require 'cucumber/rails/capybara_javascript_emulation' # Lets you click links with onclick javascript handlers without using @culerity or @javascript
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
